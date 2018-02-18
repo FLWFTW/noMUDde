17966c8cfad43a07e15cf95762f8ebc52441bfab
Will Sayin <william.sayin@gmail.com>
2018-02-18 11:51:04 +0000

    Upgrade to HTTPS
    Connection is now secure between the user and the client.

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 var express = require( 'express' );
 var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var fs = require('fs');
+var helmet = require('helmet');
+var https_opts = {
+   key:  fs.readFileSync('/etc/letsencrypt/live/__HOST__.com/privkey.pem'),
+   cert: fs.readFileSync('/etc/letsencrypt/live/__HOST__.com/cert.pem'),
+   ca:   fs.readFileSync('/etc/letsencrypt/live/__HOST__.com/chain.pem'),
+   requestCert: false,
+   rejectUnauthorized: false
+
+};
+var https = require('https').createServer( https_opts, app);
 var net = require( 'net' );
+const listenport = 8080, defHost = 'www.darkstonemud.com', defPort = 5432;
 
-const hostname = 'www.darkstonemud.com', hostport = 5432;
 
+app.use(helmet());
 app.use( express.static(__dirname + '/public') );
-http.listen( 8080, 
+https.listen( listenport,
       function()
       {
-         console.log('listening on port 8080' );
+         console.log('listening on port ' + listenport );
       });
 
+var io = require('socket.io').listen(https, {secure:true});
 io.on( 'connection',
       function( socket )
       {
+         let hostname = defHost, hostport = defPort;
+         let query = socket.handshake.query;
+         if( query.hostname != null )
+            hostname = query.hostname;
+         if( query.hostport != null )
+            hostport = query.hostport;
          var client = net.createConnection( { host:hostname, port:hostport } );
          client.setEncoding( "latin1" );
-
          client.on( 'data',
                function( data )
                {
                   socket.emit( 'data', data.toString() );
                });
-         
+
          client.on( 'close',
                function()
                {
@@ -36,26 +52,26 @@ io.on( 'connection',
          client.on( 'connect',
                function()
                {
-                  socket.emit( 'status', { host: hostname, 
-                                           port: hostport, 
+                  socket.emit( 'status', { host: hostname,
+                                           port: hostport,
                                            msg: 'connect' } );
                });
          client.on( 'error',
                function()
                {
-                  socket.emit( 'status', { host: hostname, 
-                                           port: hostport, 
+                  socket.emit( 'status', { host: hostname,
+                                           port: hostport,
                                            msg: 'error' } );
                });
 
          client.on( 'lookup',
                function()
                {
-                  socket.emit( 'status', { host: hostname, 
-                                           port: hostport, 
+                  socket.emit( 'status', { host: hostname,
+                                           port: hostport,
                                            msg: 'lookup' } );
                });
-         
+
          socket.on( 'command',
                function( cmd )
                {
